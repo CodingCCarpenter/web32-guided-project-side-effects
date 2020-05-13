bf8e17bbbacc105415ed634f511c7c452c44e3bb
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-05-13 18:55:42 +0200

    wip

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,7 +25,7 @@ export default function App() {
   useEffect(() => {
     // this code in here will run, after the first DOM surgery only
     // BASE_URL and API_KEY (http://localhost:4000/friends?api_key=xyz)
-    axios(`${BASE_URL}/friends?api_key=${API_KEY}`)
+    axios.get(`${BASE_URL}/friends?api_key=${API_KEY}`)
       .then(res => {
         setFriends(res.data)
       })
