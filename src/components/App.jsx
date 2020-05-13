a587364224c60128a3b8cafe74b8e5cc6ca8ec67
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-05-13 18:46:35 +0200

    wip

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 // TASK 1 - import the axios lib from node_modules
 import axios from 'axios'
 // TASK 2 - import the contants from constants/index.js
@@ -22,6 +22,17 @@ export default function App() {
   // caused by the first render only. You'll need `useEffect` from React.
   // The effect should consist of a call to the API using axios.
   // On success, set the array of friend objects from the API into state.
+  useEffect(() => {
+    // this code in here will run, after the first DOM surgery only
+    // BASE_URL and API_KEY (http://localhost:4000/friends?api_key=xyz)
+    axios(``)
+      .then(res => {
+
+      })
+      .catch(err => {
+        debugger
+      })
+  }, [])
 
   const Friend = (props) => {
     // const { info } = props
